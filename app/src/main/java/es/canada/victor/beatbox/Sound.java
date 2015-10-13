fa22797c0523a818273e8d93e57eab5d0fd5edfb
Víctor Cañada Ojeda <Victorcanoje@gmail.com>
2015-10-13 17:12:55 +0100

    CHAPTER 18 FINISHED

---
+++ app/src/main/java/es/canada/victor/beatbox/Sound.java
@@ -0,0 +1,26 @@
+package es.canada.victor.beatbox;
+
+/**
+ * Created by victorcanadaojeda on 13/10/15.
+ */
+public class Sound {
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length -1];
+        mName = filename.replace(".wav", "");
+    }
+
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+
+    public String getName() {
+        return mName;
+    }
+}
